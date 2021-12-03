54dc912dfc50ef30860dc2d751ff337cac925f06
mabdurrakib <mabdurrakib.dev@gmail.com>
2021-12-04 00:14:53 +0600

    product name , image , sellera name ,price added

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
   console.log(props.product.name);
+  const {name, img, seller,price,stock} = props.product;
     return (
+      <div className="product">
         <div>
-          <h4>{props.product.name}</h4>
+          <img src={img} alt="" />
         </div>
+        <div>
+          <h4 className="product-name">
+            {name}
+          </h4>
+          <br />
+          <p><small>by: {seller}</small></p>
+          <br />
+          <p>${price}</p>
+          <br />
+          <p><small>only {stock} stock available.</small></p>
+        </div>
+      </div>
+       
     );
 };
 
